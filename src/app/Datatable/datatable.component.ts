a64d694285ccc890fdd57776a6623b661d84f69d
kranthi1355 <37641030+kranthi1355@users.noreply.github.com>
2018-09-10 00:03:47 -0600

    This is the initial for datatable

---
+++ src/app/Datatable/datatable.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datatale',
+  templateUrl: './datatable.component.html',
+  styleUrls: ['./datatable.component.css']
+})
+export class DatatableComponent {
+  title = 'library';
+}
