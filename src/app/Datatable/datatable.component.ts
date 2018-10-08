6e089540f1167d62dc81e6ad3543466799077dea
kranthi1355 <37641030+kranthi1355@users.noreply.github.com>
2018-10-07 21:13:21 -0600

    small scss changes

--- src/app/Datatable/datatable.component.ts
+++ src/app/Datatable/datatable.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-datatale',
   templateUrl: './datatable.component.html',
-  styleUrls: ['./datatable.component.css']
+  styleUrls: ['./datatable.component.scss']
 })
 export class DatatableComponent {
   title = 'library';
